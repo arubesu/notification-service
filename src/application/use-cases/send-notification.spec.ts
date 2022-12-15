c313dba73f08ae10cbb3170647cce11699822434
Arubesu <bruno.arubesu@gmail.com>
2022-12-14 23:00:48 -0300

    chore: add repository pattern

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,10 +1,9 @@
-import { SendNotification } from './send-notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
-
     const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
